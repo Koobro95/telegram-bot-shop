d918e81179affebf67191961826b6d88cb006c8d
Koobro95 <70564050+Koobro95@users.noreply.github.com>
2025-07-21 16:03:13 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,6 +1,13 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.fsm.state import StatesGroup, State
 
 class OrderState(StatesGroup):
     waiting_for_name = State()
     waiting_for_phone = State()
     waiting_for_address = State()
+
+class AddProduct(StatesGroup):
+    name = State()
+    price = State()
+    size = State()
+    availability = State()
